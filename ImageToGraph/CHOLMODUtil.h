1411efc7044d1df8df9d12c8618caf0ed3ff0952
Matthew <mabennett@cs.otago.ac.nz>
2013-01-18 14:57:13 +1300

    First commit in the iterative solver branch, this will solve the layout using an iterative solver, hopefully so we can sacrifice some accuracy to get better speed.

--- ImageToGraph/CHOLMODUtil.h
+++ ImageToGraph/CHOLMODUtil.h
@@ -13,6 +13,8 @@
 @interface CHOLMODUtil : NSObject
 
 +(void) insertIntoTriplet: (cholmod_triplet *) t WithRow: (int) r col: (int) c andValue: (double) x;
++(double) cholmodDotProductOfX: (cholmod_dense *) x andY: (cholmod_dense *) y;
++(cholmod_dense *) cholmodAddDenseA: (cholmod_dense *) a andB: (cholmod_dense *) b withScalesA: (double) A andB: (double) B;
 +(cv::Mat) convertXCHOLMODDense: (cholmod_dense *) d withWidth: (int) w andHeight: (int) h;
 +(cv::Mat) convertYCHOLMODDense: (cholmod_dense *) d withWidth: (int) w andHeight: (int) h;
 +(cv::Mat) convertCHOLMODDense:(cholmod_dense *)d withWidth: (int) w andHeight: (int) h;
--- ImageToGraph/CHOLMODUtil.mm
+++ ImageToGraph/CHOLMODUtil.mm
@@ -10,6 +10,27 @@
 
 @implementation CHOLMODUtil
 
++(double) cholmodDotProductOfX: (cholmod_dense *) x andY: (cholmod_dense *) y {
+    double sum = 0.0;
+    if(x->nrow != y->nrow) {
+        NSLog(@"cannot dot these");
+        return -1;
+    }
+    for (int i = 0; i < x->nrow; i++) {
+        sum += ((double *)x->x)[i] * ((double *)y->x)[i];
+    }
+    return sum;
+}
+
++(cholmod_dense *) cholmodAddDenseA: (cholmod_dense *) a andB: (cholmod_dense *) b withScalesA: (double) A andB: (double)B{
+    cholmod_common common;
+    cholmod_dense *result = cholmod_allocate_dense(a->nrow, 1, a->nrow, CHOLMOD_REAL, &common);
+    for (int i = 0; i < a->nrow; i++) {
+        ((double *)result->x)[i] = A*((double *)a->x)[i] + B*((double *)b->x)[i];
+    }
+    return result;
+}
+
 +(void) insertIntoTriplet: (cholmod_triplet *) t WithRow: (int) r col: (int) c andValue: (double) x {
     size_t index = t->nnz++;
     ((int *)t->i)[index] = r;
